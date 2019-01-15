6b99bc9972da4b56af8ec21e9a4d2c299e7a615c
xuesongdkl <3202934553@qq.com>
2019-01-15 15:29:07 +0800

    commit

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -228,7 +228,7 @@ class AlipayController extends Controller
     {
 
 
-//        header('Refresh:2;url=/order/list');
+        header('Refresh:2;url=/order/list');
         echo "订单： ".$_GET['out_trade_no'] . ' 支付成功，正在跳转';
 
         echo '<pre>';print_r($_GET);echo '</pre>';die;
