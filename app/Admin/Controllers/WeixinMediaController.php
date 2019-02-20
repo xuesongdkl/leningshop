e752f5da45561415b48e71db049dcccd40f3a915
xuesongdkl <13233490803@163.com>
2019-02-20 12:39:50 +0000

    微信素材管理图片展示

--- app/Admin/Controllers/WeixinMediaController.php
+++ app/Admin/Controllers/WeixinMediaController.php
@@ -88,7 +88,9 @@ class WeixinMediaController extends Controller
         $grid->media_id('Media id');
         $grid->format('Format');
         $grid->msg_id('Msg id');
-        $grid->local_file_name('Local file name');
+        $grid->local_file_name('Local file name')->display(function($image){
+            return '<img src="xsdkl.52self.cn/wx/images/'.$image.'"width=100 height=100>';
+        });
         $grid->local_file_path('Local file path');
 
         return $grid;
