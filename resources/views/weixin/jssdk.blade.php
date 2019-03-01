2842880dabf3da96d63a9f4d7277b47748a437fe
xuesongdkl <13233490803@163.com>
2019-03-01 06:29:34 +0000

    debug

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -21,16 +21,16 @@
             jsApiList: ['chooseImage','uploadImage','getLocalImgData','startRecord'] // 必填，需要使用的JS接口列表
         });
         wx.ready(function(){
-//            $('#btn1').click(function(){
-//                wx.chooseImage({
-//                    count: 9, // 默认9
-//                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-//                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-//                    success: function (res) {
-//                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-//                    }
-//                });
-//            });
+            $('#btn1').click(function(){
+                wx.chooseImage({
+                    count: 9, // 默认9
+                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                    success: function (res) {
+                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    }
+                });
+            });
         });
     </script>
 @endsection
