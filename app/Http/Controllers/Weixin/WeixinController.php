a67c19863bfc5ddd663ef03e93261dfd31fc1191
xuesongdkl <13233490803@163.com>
2019-03-13 04:12:42 +0000

    fff

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -18,7 +18,7 @@ use App\Model\WeixinChatModel;
 class WeixinController extends Controller
 {
 
-//////
+
     protected $redis_weixin_access_token = 'str:weixin_access_token';     //微信 access_token
     protected $redis_weixin_jsapi_ticket = 'str:weixin_jsapi_ticket';      //微信 jsapi_ticket
 
